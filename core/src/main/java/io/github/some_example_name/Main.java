437f2bd0d01221ebed2eae6ab0a9c3b23b88c474
maltemitze <maltemitze@gmail.com>
2026-09-25 21:33:10 +0200

    Spieler Movement (Links, Rechts, Springen)

--- core/src/main/java/io/github/some_example_name/Main.java
+++ core/src/main/java/io/github/some_example_name/Main.java
@@ -2,6 +2,8 @@ package io.github.some_example_name;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -12,9 +14,8 @@ public class Main extends Game {
     private character player1;
     @Override
     public void create() {
-        setScreen(new FirstScreen());
-    }
-    {
+
+
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 480);
         batch = new SpriteBatch();
@@ -22,6 +23,9 @@ public class Main extends Game {
         player1 = new character();
         player1.setPosition(200,100);
     }
+
+
+
     public void dispose()
     {
         batch.dispose();
@@ -29,7 +33,28 @@ public class Main extends Game {
     public void render()
     {
         Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        float delta = Gdx.graphics.getDeltaTime();
+
+        // controls
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT))
+        {
+            player1.moveLeft(delta);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+        {
+            player1.moveRight(delta);
+        }
+        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE))
+        {
+            player1.jump();
+        }
+
+        // update (gravity etc.)
+        player1.update(delta);
+
+        // draw
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
         player1.draw(batch);
--- core/src/main/java/io/github/some_example_name/character.java
+++ core/src/main/java/io/github/some_example_name/character.java
@@ -13,12 +13,16 @@ public class character
     Texture texture;
     int action;
     float veolocityY;
-
+    //springen verwenden
+    float velocityY = 0;
+    float gravity = -500;   // zieht runter
+    float jumpStrength = 250; // wie stark du springst
+    boolean onGround = false;
     //Entry Point
-    public void Character()
+    public character()
     {
         bottom = new Rectangle(0.0f, 0.0f, 128.0f, 128.0f);
-        texture = new Texture(Gdx.files.internal("assets/character.png"));
+        texture = new Texture(Gdx.files.internal("character.png"));
         sprite = new Sprite(texture,0, 0, 128, 128);
         this.setPosition(0, 0);
 
@@ -40,7 +44,23 @@ public class character
     //natural movement
     public void update(float delta)
     {
+        // Gravity anwenden
+        velocityY += gravity * delta;
 
+        // Position updaten
+        setPosition(bottom.x, bottom.y + velocityY * delta);
+
+        // Boden check
+        if (bottom.y <= 0)
+        {
+            bottom.y = 0;
+            velocityY = 0;
+            onGround = true;
+        }
+        else
+        {
+            onGround = false;
+        }
     }
     public void setPosition(float x, float y)
     {
@@ -50,11 +70,21 @@ public class character
     }
     public void moveLeft(float delta)
     {
-
+        bottom.x -= (200 * delta);
+        sprite.setPosition(bottom.x, bottom.y);
     }
     public void moveRight(float delta)
     {
-
+        bottom.x += (200 * delta);
+        sprite.setPosition(bottom.x, bottom.y);
+    }
+    public void jump()
+    {
+        if (onGround)
+        {
+            velocityY = jumpStrength;
+            onGround = false;
+        }
     }
     public void draw(SpriteBatch batch)
     {
